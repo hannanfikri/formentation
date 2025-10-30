3e4da42f6f5534c050db44a0402f403eb9f5336a
Hannan Fikri <hannanfikri510@gmail.com>
2025-10-30 15:24:38 +0800

    add submit button

--- src/pages/PersonalDetails.tsx
+++ src/pages/PersonalDetails.tsx
@@ -2,17 +2,26 @@ import { PersonalDetailsForm } from "@/components/forms";
 import { FormProvider } from "react-hook-form";
 import { usePersonalDetails } from "../hooks";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
 
 const PersonalDetails: React.FC = () => {
   const { formMethods, onSubmit } = usePersonalDetails();
   return (
     <Card className="w-full">
-      <CardHeader className="text-4xl font-bold">Personal Details Form</CardHeader>
+      <CardHeader className="text-4xl font-bold">
+        Personal Details Form
+      </CardHeader>
       <CardContent>
         <FormProvider {...formMethods}>
           <form onSubmit={formMethods.handleSubmit(onSubmit)}>
             <PersonalDetailsForm />
           </form>
+
+          <Button type="submit" className="flex gap-2">
+            <span>Next Page</span>
+            <ArrowRightIcon />
+          </Button>
         </FormProvider>
       </CardContent>
     </Card>
